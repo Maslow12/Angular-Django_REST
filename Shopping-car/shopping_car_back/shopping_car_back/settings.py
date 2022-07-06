48c399a5c3ddd96d60a2dbb47224aa4f22365547
Sebastian <40075601+Maslow12@users.noreply.github.com>
2022-07-06 16:45:08 -0400

    Update settings.py

--- Shopping-car/shopping_car_back/shopping_car_back/settings.py
+++ Shopping-car/shopping_car_back/shopping_car_back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@ejp)*6ix7i+*k7vrv-7g=wn6=in-0-ea335*tdlbdmci$k#5'
+SECRET_KEY = '' #Secret key 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
